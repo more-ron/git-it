58ae5910b501d377818e7d32a4d188275a5eb4fe
MoreRon <ronald.l.maravilla@gmail.com>
2013-02-18 07:02:16 +0800

    Some docs

--- lib/git-it.rb
+++ lib/git-it.rb
@@ -1,7 +1,22 @@
+# =================================
+# = Current version of the matrix =
+# =================================
 require 'git-it/version'
-require 'git-it/global_helper'
+
+# ==================================
+# = Does the commading around here =
+# ==================================
 require 'git-it/commander'
+
+# ================
+# = Links to web =
+# ================
 require 'git-it/link_generator'
+
+# ================================
+# = Not much of a help right not =
+# ================================
+require 'git-it/global_helper'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
